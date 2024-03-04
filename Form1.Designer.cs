9b80537e52f10ef61efc20809898aa119cc009ff
Cole Zill <98670265+Cole-Z@users.noreply.github.com>
2024-03-04 16:48:23 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace UnblockResxFiles
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.browseButton = new System.Windows.Forms.Button();
+			this.directoryTextBox = new System.Windows.Forms.TextBox();
+			this.unblockButton = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.btnProgressLbl = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// browseButton
+			// 
+			this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.browseButton.Location = new System.Drawing.Point(49, 55);
+			this.browseButton.Name = "browseButton";
+			this.browseButton.Size = new System.Drawing.Size(84, 26);
+			this.browseButton.TabIndex = 0;
+			this.browseButton.Text = "Open Dir";
+			this.browseButton.UseVisualStyleBackColor = true;
+			this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+			// 
+			// directoryTextBox
+			// 
+			this.directoryTextBox.BackColor = System.Drawing.SystemColors.Menu;
+			this.directoryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.directoryTextBox.Location = new System.Drawing.Point(142, 57);
+			this.directoryTextBox.Multiline = true;
+			this.directoryTextBox.Name = "directoryTextBox";
+			this.directoryTextBox.Size = new System.Drawing.Size(219, 20);
+			this.directoryTextBox.TabIndex = 1;
+			this.directoryTextBox.TextChanged += new System.EventHandler(this.directoryTextBox_TextChanged);
+			// 
+			// unblockButton
+			// 
+			this.unblockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.unblockButton.Location = new System.Drawing.Point(49, 105);
+			this.unblockButton.Name = "unblockButton";
+			this.unblockButton.Size = new System.Drawing.Size(84, 27);
+			this.unblockButton.TabIndex = 2;
+			this.unblockButton.Text = "Unblock";
+			this.unblockButton.UseVisualStyleBackColor = true;
+			this.unblockButton.Click += new System.EventHandler(this.unblockButton_Click_1);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(49, 138);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(312, 23);
+			this.progressBar1.TabIndex = 3;
+			this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+			// 
+			// btnProgressLbl
+			// 
+			this.btnProgressLbl.AutoSize = true;
+			this.btnProgressLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnProgressLbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.btnProgressLbl.Location = new System.Drawing.Point(139, 114);
+			this.btnProgressLbl.Name = "btnProgressLbl";
+			this.btnProgressLbl.Size = new System.Drawing.Size(56, 20);
+			this.btnProgressLbl.TabIndex = 4;
+			this.btnProgressLbl.Text = "Status";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(65)))));
+			this.ClientSize = new System.Drawing.Size(411, 188);
+			this.Controls.Add(this.btnProgressLbl);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.unblockButton);
+			this.Controls.Add(this.directoryTextBox);
+			this.Controls.Add(this.browseButton);
+			this.Name = "Form1";
+			this.Text = "Resx Restore";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button browseButton;
+		private System.Windows.Forms.TextBox directoryTextBox;
+		private System.Windows.Forms.Button unblockButton;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label btnProgressLbl;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace UnblockResxFiles
+{
+	public partial class Form1 : Form
+	{
+		private Timer timer;
+		private int progressBarMaxValue = 100;
+		private int progressBarIncrement = 10; 
+		private int timerInterval = 300; // 3 seconds
+		private string directoryPath;
+
+		public Form1()
+		{
+			InitializeComponent();
+			InitializeTimer();
+		}
+
+		private void InitializeTimer()
+		{
+			timer = new Timer();
+			timer.Interval = timerInterval;
+			timer.Tick += Timer_Tick;
+		}
+
+		private void browseButton_Click(object sender, EventArgs e)
+		{
+			using (var folderBrowserDialog = new FolderBrowserDialog())
+			{
+				if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+				{
+					directoryPath = folderBrowserDialog.SelectedPath;
+					directoryTextBox.Text = directoryPath;
+				}
+			}
+		}
+
+		private void UnblockResxFiles(string directoryPath)
+		{
+			string[] resxFiles = Directory.GetFiles(directoryPath, "*.resx", SearchOption.AllDirectories);
+
+			foreach (string resxFile in resxFiles)
+			{
+				UnblockFile(resxFile);
+			}
+		}
+
+		// unblock the path to the folder containg the .resx file with pwrshell
+		private void UnblockFile(string filePath)
+		{
+			try
+			{
+				ProcessStartInfo psi = new ProcessStartInfo("powershell.exe")
+				{
+					Arguments = $"-Command \"Unblock-File '{filePath}'\"",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				};
+
+				Process process = Process.Start(psi);
+				process.WaitForExit();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Error unblocking file {filePath}: {ex.Message}");
+				btnProgressLbl.Text = "Failed";
+				btnProgressLbl.BackColor = Color.Salmon;
+			}
+		}
+
+
+		private void unblockButton_Click_1(object sender, EventArgs e)
+		{
+			string directoryPath = directoryTextBox.Text;
+
+			if (string.IsNullOrEmpty(directoryPath) || !Directory.Exists(directoryPath))
+			{
+				MessageBox.Show("Please select a valid directory.");
+				return;
+			}
+
+			// check if directory contains .resx files
+			string[] resxFiles = Directory.GetFiles(directoryPath, "*.resx", SearchOption.AllDirectories);
+			if (resxFiles.Length == 0)
+			{
+				btnProgressLbl.ForeColor= Color.Salmon;
+				btnProgressLbl.Text = "Incorrect Folder";
+				MessageBox.Show("The selected directory does not contain any .resx files.");
+				return;
+			}
+			progressBar1.Value = 0; 
+			timer.Start();
+		}
+
+
+		private void progressBar1_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			btnProgressLbl.Text = "Searching Files";
+			progressBar1.BackColor = Color.Blue;
+
+			if (progressBar1.Value < progressBarMaxValue)
+			{
+				progressBar1.Value += progressBarIncrement;
+			}
+			else
+			{
+				timer.Stop();
+
+				UnblockResxFiles(directoryPath);
+				MessageBox.Show("Unblocking complete.");
+				btnProgressLbl.Text = "Complete";
+				btnProgressLbl.ForeColor = Color.Lime;
+			}
+		}
+
+		private void directoryTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
